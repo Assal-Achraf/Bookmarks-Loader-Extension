5a63cfb542ffc38de3150d067ea3cf7c3e4f4e17
Assal-Achraf <contact.achrafassal@gmail.com>
2024-03-12 10:59:42 +0000

    starting puting the logic for geting all the bookmarks dinamkly

--- background.js
+++ background.js
@@ -21,15 +21,42 @@
 
 // =======================================
 
+function GetCheldrenNode(bookmark,index) {
+  console.log("enter",index);
+  console.log("bookmark id",bookmark.id);
+  if (!bookmark.hasOwnProperty("url")) {
+    chrome.bookmarks.getChildren(bookmark.id,function (_bookmarks) {
+      console.log(index,_bookmarks);
+    });
+  } 
+}
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'getBookmarks') {
-    chrome.bookmarks.getChildren(
-      "2",
-      function(bookmarks) {
-        sendResponse(bookmarks); 
-      }
-    ) 
-      return true; 
+    // chrome.bookmarks.getChildren(
+    //   "2",
+    //   function(bookmarks) {
+    //     sendResponse(bookmarks); 
+    //   }
+    // ) 
+    //   return true; 
+
+    // =======================================
+
+    // chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
+    //          console.log(bookmarkTreeNodes);
+
+    //        });
+
+    // ===========================================
+
+    chrome.bookmarks.get(["0","1","2"],function (bookmarks) {
+      console.log(bookmarks);
+      bookmarks.forEach((bookmark,index)=>{
+        GetCheldrenNode(bookmark,index)
+      });
+    })
+
   }
 });
 
--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 chrome.runtime.sendMessage({ action: 'getBookmarks' }, (bookmarks) => {
     console.log(2,"action has beensent");
     const ul = document.getElementById('bookmarksList');
-    bookmarks.forEach((bookmark) => {
-        const li = document.createElement('li');
-        li.textContent = bookmark.title;
-        ul.appendChild(li);
-    });
+    // bookmarks.forEach((bookmark) => {
+    //     const li = document.createElement('li');
+    //     li.textContent = bookmark.title;
+    //     ul.appendChild(li);
+    // });
 });
 
