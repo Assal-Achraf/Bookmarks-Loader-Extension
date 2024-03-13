9559bf59f6961c47804bd10f7a78bb1525461d2a
Assal-Achraf <contact.achrafassal@gmail.com>
2024-03-13 15:12:12 +0000

    add a button for the user to download the bookmars , and fixing the issu of lunshing actions evre time the user open a new window

--- content.js
+++ content.js
@@ -1,10 +1,8 @@
-chrome.runtime.sendMessage({ action: 'getBookmarks' }, (bookmarks) => {
-    console.log(2,"action has beensent");
-    const ul = document.getElementById('bookmarksList');
-    // bookmarks.forEach((bookmark) => {
-    //     const li = document.createElement('li');
-    //     li.textContent = bookmark.title;
-    //     ul.appendChild(li);
-    // });
-});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const exportBtn = document.getElementById('exportBtn');
+    exportBtn.addEventListener('click', () => {
+      chrome.runtime.sendMessage({ action: 'getBookmarks' }, (bookmarks) => {});
+    });
+  });
 
